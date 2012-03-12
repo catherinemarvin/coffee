b7e8774e344250c427dbf1ab53a1cac9a2998314
Kevin Hwang <k3vinhwang@gmail.com>
2012-03-12 03:07:26 -0700

    Laying down the piping

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var nowjs = require('now');
+var redis = require('redis')
+
+server = express.createServer();
+
+var everyone = nowjs.initialize(server, {socketio:{"log level": process.argv[2]}});
+
+var client = redis.createClient()
+
+server.set('view options', {
+	layout: false
+});
+
+server.set('view engine', 'ejs');
+
+// Configuration
+server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+server.set('views', __dirname + '/views');
+server.use("/static", express.static(__dirname + '/static'));
+
+server.get('/', function (req, res){
+	res.render("index");
+});
+
+server.get('/manage', function (req, res) {
+	res.render("manage");
+});
+
+server.listen(80);
+console.log("Express server listening on port %d", server.address().port);
